0265f2d1daef9ab4b28120b74e823ebfbaa78ed3
Owlz <whootandahalf@gmail.com>
2017-10-26 22:12:58 -0400

    Fixing docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,8 +19,11 @@
 #
 import os
 import sys
+
+here = os.path.dirname(os.path.realpath(__file__))
+
+sys.path.insert(0, os.path.abspath(os.path.join(here,'..','..')))
 from Gallimaufry.Version import version
-# sys.path.insert(0, os.path.abspath('.'))
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
